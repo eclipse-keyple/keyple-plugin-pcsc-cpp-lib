bfc4544859b7507a1e6ca5239d5769240840247e
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2023-01-03 18:57:47 +0100

    ci: fix sed issue

--- .github/scripts/patch_doxyfile.sh
+++ .github/scripts/patch_doxyfile.sh
@@ -6,4 +6,5 @@ sed -i "s/%PROJECT_VERSION%/$project_version/g" ./.github/doxygen/Doxyfile
 
 project_name="$(head -n 1 README.md | sed 's/#//')"
 
-sed -i "s/%PROJECT_NAME%/$project_name/g" ./.github/doxygen/Doxyfile
+# use '^' as sed separator to allow '/' in project name.
+sed -i "s^%PROJECT_NAME%^$project_name^g" ./.github/doxygen/Doxyfile
