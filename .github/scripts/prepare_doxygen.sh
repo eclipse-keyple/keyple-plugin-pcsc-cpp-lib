2a548b4eaca9501df5a4414abf4beb246ab26ac0
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2023-06-22 22:24:25 +0200

    Org: updated build system
    - fixed documentation sorting
    - prepared next release

--- .github/scripts/prepare_doxygen.sh
+++ .github/scripts/prepare_doxygen.sh
@@ -23,7 +23,7 @@ cp -rf ../.github/doxygen/out/html/* "$project_version"/
 echo "Update versions list..."
 echo "| Version | Documents |" >list_versions.md
 echo "|:---:|---|" >>list_versions.md
-for directory in $(ls -rd [0-9]*/ | cut -f1 -d'/'); do
+for directory in $(ls -rvd [0-9]*/ | sort -t. -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr | cut -f1 -d'/'); do
   echo "| $directory | [API documentation]($directory) |" >>list_versions.md
 done
 
@@ -31,9 +31,11 @@ echo "Computed all versions:"
 cat list_versions.md
 
 echo "Update project name to $project_name"
-# use '^' as sed separator to allow '/' in project name.
-sed -i "2s^.*^title: $project_name^" _config.yml
+sed -i "2s/.*/title: $project_name/" _config.yml
 
 cd ..
 
 echo "Local docs update finished."
+
+
+
